cc80b5a6039fb3c20d7e5c5100fbe8da07163e58
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-09-07 22:16:02 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE='repair-hub-gamer-pro-status-card-v2';
+const CACHE='repair-hub-gamer-pro-v2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
