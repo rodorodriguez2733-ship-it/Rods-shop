524f077cb5106ee27a3a6db889744dee9097c3f4
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-09-07 22:46:49 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
-const CACHE='repair-hub-v6';
+const CACHE='repair-hub-v7';
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{ if(e.request.mode==='navigate'){ e.respondWith(fetch(e.request)); } });
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request,{cache:'no-store'}));
+  }
+});
