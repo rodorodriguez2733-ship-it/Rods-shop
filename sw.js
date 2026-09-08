349c09131dc2aadb3d2f6b51f332e523ff0e2cb2
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-09-07 23:17:06 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='repair-hub-v10';
+const CACHE='repair-hub-v11';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
