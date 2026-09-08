8e878c820fd1d5beabd0f02730fce71064ad9793
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-09-07 20:04:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const C='repair-tradein-items-v1';
+const C='repair-tradein-modern-v2';
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
