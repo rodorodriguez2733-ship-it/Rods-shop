f92e99924f293618d57408c41c3b52f1bb23e169
rodorodriguez2733-ship-it <rodorodriguez2733@gmail.com>
2026-09-07 21:48:28 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const C='repair-tradein-brand-condition-v3';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
+const CACHE='repair-hub-gamer-pro-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
